4b776952bcf520902066ab68e6c45a0e1e4fa6aa
diego <diego.iraheta@tadeosystems.com>
2021-02-04 09:55:24 -0600

    primer commit

---
+++ FrontEndLibro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FrontEndLibro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
